b5d993198b6843c1807eeba56fc8572aae00558e
Aditya Pathak <68177924+Phoenix1504e@users.noreply.github.com>
2023-08-05 17:43:15 +0530

    Update Solver.py

--- Solver.py
+++ Solver.py
@@ -1,1 +1,22 @@
+def solveSudoku(board):
+    def is_valid(row, col, num):
+        for i in range(9):
+            if board[row][i] == num or board[i][col] == num or board[3 * (row // 3) + i // 3][3 * (col // 3) + i % 3] == num:
+                return False
+        return True
 
+    def solve():
+        for row in range(9):
+            for col in range(9):
+                if board[row][col] == 0:
+                    for num in range(1, 10):
+                        if is_valid(row, col, num):
+                            board[row][col] = num
+                            if solve():
+                                return True
+                            board[row][col] = 0
+                    return False
+        return True
+
+    solve()
+    return board
